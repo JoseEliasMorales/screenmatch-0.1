35510f0a6ddeac2033e1ea79b1bc60aed299db54
joselias <joselias.dev@gmail.com>
2024-11-05 11:02:33 -0300

    first commit

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto){
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
